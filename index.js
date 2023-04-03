1e793bff5e55c95df2d33e983740c409c2fc00fb
+ <+>
2023-04-03 15:37:30 -0400

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = 5;
+
+Math.floor(Math.random() * 5) +1;
+
+let num3 = 8
+let num4 = 2
+const mod = (num3/num4);
+
+let max = (1, 2,10,16,18,20);
